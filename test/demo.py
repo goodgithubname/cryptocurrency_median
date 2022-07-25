396df0efc465a8d273e61ee46a635519c08a6206
unknown <darkkiller.sudsawad@gmail.com>
2022-07-25 18:34:25 +0700

    Test cases from json file

--- test/demo.py
+++ test/demo.py
@@ -1,17 +1,19 @@
 # python libraries
 import asyncio
+import json
 
 # modules
 from mypackage.calculations.functions import *
 from mypackage.handler import *
 
-# test cases
-from testcase import *
+# load test_cases.json
+with open("test_cases.json", "r") as j:
+    data = json.load(j)
 
 
 if __name__ == "__main__":
-    for i in range(0, len(test_cases) - 1):
-        print(f"TEST CASE: {i+1}")
+    for i in data:
+        print(f"TEST CASE: {i}")
         print("===============================")
-        print(asyncio.run(get_exchange_prices(test_cases[i])))
+        print(asyncio.run(get_exchange_prices(data[i])))
         print(f"\n===============================\n")
